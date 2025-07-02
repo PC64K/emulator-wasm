1e0ce9ef143c02b8765ad72cba2cb499a17f6ffc
Milk-Cool <coder.tomsk@gmail.com>
2025-07-02 16:34:18 +0300

    keyboard input demo

--- include/emu/config.h
+++ include/emu/config.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define CPU_SPEED_HZ 10000 // Up to 1 MHz, will stop working with higher values // FIXME?
+#define CPU_SPEED_HZ 100000 // Up to 1 MHz, will stop working with higher values // FIXME?
